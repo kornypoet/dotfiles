13758a6bdaca5ab7f101a690578d367c742c539a
Travis Dempsey <dempsey.travis@gmail.com>
2017-06-16 17:50:58 -0500

    overhaul aliases and functions

--- zsh.d/alias.sh
+++ zsh.d/alias.sh
@@ -9,74 +9,80 @@ alias ls="ls -FG"
 # Show hidden files with human-readable size in list form
 alias la="ls -lah"
 
-# Open Mac OSX emacs
-if [[ -x /usr/local/Cellar/emacs/24.5/bin ]] ; then
-  alias gmacs="/usr/local/Cellar/emacs/24.5/Emacs.app/Contents/MacOS/Emacs"
-  alias emacs="/usr/local/Cellar/emacs/24.5/bin/emacs"
-  alias emacsclient="/usr/local/Cellar/emacs/24.5/bin/emacsclient"
+# Emacs alias
+alias e="emacs"
+
+# Use brew installed emacs if it exists
+if [[ -x /usr/local/Cellar/emacs/25.2/bin ]] ; then
+    alias gmacs="/usr/local/Cellar/emacs/25.2/bin/emacs"
+    alias emacs="/usr/local/Cellar/emacs/25.2/bin/emacs -nw"
+    alias emacsclient="/usr/local/Cellar/emacs/25.2/bin/emacsclient"
 fi
 
 if [ -n "$INSIDE_EMACS" ]; then
-  alias emacs="emacs-within-emacs"
+    alias emacs="emacs-within-emacs"
 fi
 
-function emacsd() {
-  if [ -z "$(ps aux | grep -p '[Ee]macs.*--daemon')" ] ; then
-    emacs --daemon
-  fi
+# Start emacsclient as a terminal and start server if not running
+function et() {
+    emacsclient -t --eval "(progn (multi-term) (rename-buffer \"${1:-jj}\"))" -a ""
 }
 
-function emacst() {
-  emacsclient -t --eval "(multi-term)"
-}
-
+# When inside emacs open a new buffer instead of a frame
 function emacs-within-emacs() {
-  emacsclient -n "$@"
+    emacsclient -n "$@"
 }
 
 # Make directory navigation easier
 alias ..="cd ../"
 alias ...="cd ../.."
 alias ....="cd ../../.."
+alias .....="cd ../../../.."
 
 # Vagrant alias
 alias v="vagrant"
 
 # Useful bundle shorcuts
+alias b="bundle"
 alias brb="bundle console"
 
-alias b="bundle"
-
-function bx() {
-  bundle exec "$@"
+function be() {
+    bundle exec "$@"
 }
 
 function bk() {
-  bundle exec knife "$@"
+    bundle exec knife "$@"
 }
 
 function bake() {
-  bundle exec rake "$@"
+    bundle exec rake "$@"
 }
 
 function bspec() {
-  bundle exec rspec "$@"
+    bundle exec rspec "$@"
 }
 
 function bcuc() {
-  bundle exec cucumber "$@"
+    bundle exec cucumber "$@"
 }
 
-# Git and gist shortcuts
-alias g="git"
+# Git related shortcuts
+alias g="hub"
 
+# Create a gist from a file, name it after that file, and copy URL to clipboard
 function gst() {
-  gist -c -f "$@" < "$@"
+    gist -c -f "$@" < "$@"
 }
 
 # Random alphanumeric password generator
 # Specify an output length as an argument, defaults to 8
 function randpwd() {
-  length=${1:-8}
-  openssl rand -base64 128 | tr -cd "[:alnum:]" | head -c $length ; echo
+    length=${1:-8}
+    openssl rand -base64 128 | tr -cd "[:alnum:]" | head -c $length ; echo
+}
+
+function eyedit() {
+    eyaml edit --pkcs7-private-key $SEC_DIR/eyaml_keys/$1_private_key.pkcs7.pem \
+	  --pkcs7-public-key $SEC_DIR/eyaml_keys/$1_public_key.pkcs7.pem \
+	  $PUPPET_MODULES_DIR/environments/hieradata/$1_environment.eyaml
 }
